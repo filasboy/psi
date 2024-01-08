3b8eed6018f7c1f13551ba37af5aee49f29653eb
filasboy <filddias07@gmail.com>
2024-01-08 10:35:42 +0000

    fazer exerciciio quatro da semana09

--- semana09/testeerros/Program.cs
+++ semana09/testeerros/Program.cs
@@ -6,12 +6,22 @@ namespace testeerros
     {
         static void Main(string[] args)
         {
-            Console.Write(“Insere um número inteiro: ”);
-            int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($“Número inserido: {i}”);
+          int i = 0;
+          Console.WriteLine("Insere um número inteiro: ");
+            try 
+           {
+           
+                // Código perigoso que pode lançar exceções
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                // Código para tratar exceção
+                 Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
+      
+            }   
 
-       
-          
-          }
-    }
+              Console.WriteLine($"Número inserido: {i}");
+        }
+    }  
 }
