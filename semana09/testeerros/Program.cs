0a60c845891ef02775f7ee7c0c8f2f2c8ad4f0b5
filasboy <filddias07@gmail.com>
2024-01-08 09:19:20 +0000

    fazer exerciciio tres da semana09

---
+++ semana09/testeerros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace testeerros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(“Insere um número inteiro: ”);
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($“Número inserido: {i}”);
+
+       
+          
+          }
+    }
+}
